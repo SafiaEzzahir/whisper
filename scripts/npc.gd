716885ad52558ca56b71295798f26a1112a029a0
SafiaEzzahir <safia.ezzahir@hotmail.com>
2026-03-29 23:50:45 +0100

    View friend stats by hovering on each npc

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -1,9 +1,13 @@
 extends Node
 
+const npc_stats_scene = preload("res://components/characters/npc_stats.tscn")
+
 signal change_scene
 
 var time_since_entered = 0
 var counting = false
+
+var stats = null
 
 var time_limit = 2
 
@@ -13,7 +17,8 @@ func _process(delta: float):
 	if counting:
 		time_since_entered += delta
 		if time_since_entered >= time_limit:
-			enter_npc()
+			if not stats:
+				enter_npc()
 			#get_tree().change_scene_to_file("res://scenes/game.tscn")
 			#Game.disinstantiate_char_sprite()
 	
@@ -25,6 +30,13 @@ func _on_body_entered(_body: Node2D) -> void:
 func _on_body_exited(_body: Node2D) -> void:
 	counting = false
 	time_since_entered = 0
+	if stats:
+		remove_child(stats)
+		stats = null
 	
 func enter_npc():
-	pass
+	stats = npc_stats_scene.instantiate()
+	stats.position = $body.position
+	print(npc_name)
+	stats.get_node("name").text = npc_name
+	add_child(stats)
--- scripts/npc_stats.gd
+++ scripts/npc_stats.gd
@@ -1,1 +1,16 @@
 extends Node
+
+const stat_label = preload("res://components/characters/npc_stat_label.tscn")
+
+var self_index = 0 #change but howwww??
+var spacing = 40
+
+func _ready():
+	var friends = Game.chars[self_index]
+	for friend in friends:
+		if not friend == "name":
+			spacing += 20
+			var lbl = stat_label.instantiate()
+			add_child(lbl)
+			lbl.global_position = Vector2i(self.global_position.x, self.global_position.y+spacing)
+			lbl.text = friend + ": " + str(Game.chars[self_index][friend])
