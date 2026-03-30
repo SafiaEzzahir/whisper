95492f54fbb6bfac52ecad1fd9690e302b61c36d
SafiaEzzahir <safia.ezzahir@hotmail.com>
2026-03-30 13:47:46 +0100

    Add hints

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,12 @@
 extends CharacterBody2D
 
+const hintnode = preload("res://components/player/hint.tscn")
+
 var speed = 300
+var hint_created = false
+
+func _ready():
+	$hint.pressed.connect(hint_func)
 
 func _process(delta: float) -> void:
 	_move(delta)
@@ -12,3 +18,11 @@ func _move(delta):
 	velocity = position_change * speed
 	move_and_slide()
 	return position_change
+
+func hint_func():
+	if not hint_created:
+		hint_created = hintnode.instantiate()
+		$hint.add_child(hint_created)
+	else:
+		$hint.remove_child(hint_created)
+		hint_created = false
